3dfe98444a5e8b686a2f5b55234585884d83ed63
fatonirizki <fatonirizki12@gmail.com>
2022-10-14 17:33:03 +0700

    backend file login web desa cikahuripan

---
+++ index.js
@@ -0,0 +1,17 @@
+import express  from "express";
+import dotenv from "dotenv";
+import db from "./config/Database.js";
+import router from "./routes/index.js";
+dotenv.config();
+const app = express();
+try{
+    await db.authenticate();
+    console.log('Database Connected....');
+}
+catch(error){
+    console.error(error);
+}
+app.use(express.json());
+app.use(router);
+app.listen(5000, ()=> console.log('Server Running at Port 5000'));
+
